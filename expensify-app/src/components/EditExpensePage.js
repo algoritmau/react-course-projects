797f6122bc46394f69b48158621dad5441577dc2
Mauricio Paternina <spaceinvadev@gmail.com>
2020-10-06 06:51:26 -0500

    Organize components files structure

---
+++ expensify-app/src/components/EditExpensePage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EditExpensePage = () => (
+  <div>
+    This is from my <code>EditExpensePage</code> component
+  </div>
+);
+
+export default EditExpensePage;
--- expensify-app/src/routers/AppRouter.js
+++ expensify-app/src/routers/AppRouter.js
@@ -1,47 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, NavLink, Link } from 'react-router-dom';
-
-const ExpenseDashboardPage = () => (
-  <div>
-    This is from my <code>ExpenseDashboardPage</code> component
-  </div>
-);
-
-const AddExpensePage = () => (
-  <div>
-    This is from my <code>AddExpensePage</code> component
-  </div>
-);
-
-const EditExpensePage = () => (
-  <div>
-    This is from my <code>EditExpensePage</code> component
-  </div>
-);
-
-const HelpPage = () => (
-  <div>
-    This is from my <code>HelpPage</code> component
-  </div>
-);
-
-const NotFoundPage = () => (
-  <div>
-    404! - <Link to="/">← Back to Home</Link>
-  </div>
-);
-
-const Header = () => (
-  <header>
-    <h1>Expensify</h1>
-    <nav>
-      <NavLink exact to="/" activeClassName="is-active">Dashboard</NavLink>
-      <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-      <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
-      <NavLink to="/help" activeClassName="is-active">Help</NavLink>
-    </nav>
-  </header>
-);
+import Header from '../components/Header';
+import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
+import AddExpensePage from '../components/AddExpensePage';
+import EditExpensePage from '../components/EditExpensePage';
+import HelpPage from '../components/HelpPage';
+import NotFoundPage from '../components/NotFoundPage';
 
 const AppRouter = () => (
   <BrowserRouter>
